86c8c1745daec793cf0f145e4025b560f1adfd9e
mxholt <mxholt@DESKTOP-K1T3GIL>
2017-03-07 14:29:43 -0600

    calling the api

--- FrontEnd/FrontEnd/Controllers/HomeController.cs
+++ FrontEnd/FrontEnd/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using FrontEnd.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,14 +13,28 @@ namespace FrontEnd.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+       
+
+    
+    public async Task<ActionResult> Index()
         {
+
+             //  Get("sss");
+
+       //     MakeRequest();
+        var blah = await GetBeerAsync("api/beer");
+
+       
+
+
             var model = new SplashPageViewModel();
-            model.TopFiveBeers.Add(new Beer { Name = "Beer 1" });
-            model.TopFiveBeers.Add(new Beer { Name = "Beer 2" });
-            model.TopFiveBeers.Add(new Beer { Name = "Beer 3" });
-            model.TopFiveBeers.Add(new Beer { Name = "Beer 4" });
-            model.TopFiveBeers.Add(new Beer { Name = "Beer 5" });
+        model.TopFiveBeers = blah;
+            //model.TopFiveBeers.Add(new Beer { Name = "Beer 1" });
+            //model.TopFiveBeers.Add(new Beer { Name = "Beer 2" });
+            //model.TopFiveBeers.Add(new Beer { Name = "Beer 3" });
+            //model.TopFiveBeers.Add(new Beer { Name = "Beer 4" });
+            //model.TopFiveBeers.Add(new Beer { Name = "Beer 5" });
 
             model.TopFivePizzas.Add(new Pizza { Name = "Pizza 1" });
             model.TopFivePizzas.Add(new Pizza { Name = "Pizza 2" });
@@ -26,6 +43,53 @@ namespace FrontEnd.Controllers
             model.TopFivePizzas.Add(new Pizza { Name = "Pizza 5" });
             return View(model);
         }
+
+        static async Task<List<Beer>> GetBeerAsync(string path)
+        {
+             HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://socialbeerdiawebapi.azurewebsites.net/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            List<Beer> beers = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                beers = await response.Content.ReadAsAsync<List<Beer>>();
+            }
+            client.Dispose();
+            return beers;
+        }
+        //static async void MakeRequest()
+        //{
+        //    var client = new HttpClient();
+        //    var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+        //    // Request headers
+        //    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "dd5c6e92a043401a9c5a26c4e60938d0");
+        //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        //    var uri = "http://beerdia.azure-api.net/beer/beer/?" + queryString;
+
+        //    var response = await client.GetAsync(uri);
+        //}
+
+
+
+        //public Beer Get(string id)
+        //{
+        //    var rtn = default(Beer);
+
+        //    var task = Task.Run(() => GetBeerAsync("api/beer"));
+        //    var result = task.Result;
+        //    //var status = result?.Status;
+        //    //if (!string.IsNullOrEmpty(status) && status == "success")
+        //    //{
+        //    //    rtn = result.Data;
+        //    //}
+
+        //    return rtn;
+        //}
 
         public ActionResult About()
         {
--- FrontEnd/FrontEnd/Models/Beer.cs
+++ FrontEnd/FrontEnd/Models/Beer.cs
@@ -2,11 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
 
 namespace FrontEnd.Models
 {
-    public class Beer
+    public class Beer 
     {
+       // public Beer(IGlass glass, ISrm srm, IAvailable available, IStyle style, List<Brewery> breweries, ILabels labels, List<SocialAccount> socialAccounts);
+
+        public double Abv { get; set; }
+     //   [JsonConverter(typeof(ConcreteConverter<Available>))]
+     //   public IAvailable Available { get; set; }
+        public int AvailableId { get; set; }
+        public List<Brewery> Breweries { get; set; }
+        public string Brewery { get; }
+        public string CreateDate { get; set; }
+        public string Description { get; set; }
+      //  [JsonConverter(typeof(ConcreteConverter<Glass>))]
+     //   public IGlass Glass { get; set; }
+        public int GlasswareId { get; set; }
+        public string Id { get; set; }
+        public string IsOrganic { get; set; }
+      //  [JsonConverter(typeof(ConcreteConverter<Labels>))]
+      //  public ILabels Labels { get; set; }
         public string Name { get; set; }
+        public string NameDisplay { get; set; }
+        public string ServingTemperature { get; set; }
+      //  public List<ISocialAccount> SocialAccounts { get; set; }
+      //  [JsonConverter(typeof(ConcreteConverter<Srm>))]
+     //   public ISrm Srm { get; set; }
+        public int SrmId { get; set; }
+        public string Status { get; set; }
+        public string StatusDisplay { get; set; }
+      //  [JsonConverter(typeof(ConcreteConverter<Style>))]
+      //  public IStyle Style { get; set; }
+        public int StyleId { get; set; }
+        public string UpdateDate { get; set; }
+    }
+
+
+    public class Brewery 
+    {
+        public string CreateDate { get; set; }
+        public string Description { get; set; }
+        public string Established { get; set; }
+        public string Id { get; set; }
+     //   public IImages Image { get; set; }
+        public string IsOrganic { get; set; }
+      //  public List<Location> Locations { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string StatusDisplay { get; set; }
+        public string UpdateDate { get; set; }
+        public string Website { get; set; }
     }
 }
