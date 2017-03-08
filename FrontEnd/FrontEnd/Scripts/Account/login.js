37914bce04c0021bf7a25ababa80080eb137113c
acmitchelllkq <acmitchell@lkqcorp.com>
2017-03-08 10:07:15 -0600

    Session storage

--- FrontEnd/FrontEnd/Scripts/Account/login.js
+++ FrontEnd/FrontEnd/Scripts/Account/login.js
@@ -3,10 +3,14 @@
 function onSignIn(googleUser) {
     //var profile = googleUser.getBasicProfile();
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log('ID: ' + id_token); // Do not send to your backend! Use an ID token instead.
     //console.log('Name: ' + profile.getName());
     //console.log('Image URL: ' + profile.getImageUrl());
     //console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    if (typeof (Storage) !== "undefined") {
+        sessionStorage.authToken = id_token;
+        console.log('Token: ' + sessionStorage.authToken);
+    }
 }
 
 function onError(googleUser) {
